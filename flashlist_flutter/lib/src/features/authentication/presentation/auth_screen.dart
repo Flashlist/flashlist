b7e50b422ca52f9a29a7da3ce66f4e642965bbfb
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-19 13:12:10 +0100

    add basic screens

---
+++ flashlist_flutter/lib/src/features/authentication/presentation/auth_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:serverpod_auth_email_flutter/serverpod_auth_email_flutter.dart';
+
+import 'package:flashlist_flutter/src/features/serverpod/application/serverpod_controller.dart';
+
+class AuthScreen extends ConsumerWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final serverpodController = ref.watch(serverpodControllerProvider);
+
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SignInWithEmailButton(
+          caller: serverpodController.client.modules.auth,
+          onSignedIn: () {},
+        )
+      ],
+    ));
+  }
+}
