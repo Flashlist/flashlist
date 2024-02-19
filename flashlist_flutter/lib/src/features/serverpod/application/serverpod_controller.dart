2f199379ec0c8cd3037ad79d79e19b42c275c5a3
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-19 13:12:00 +0100

    add serverpod controller

---
+++ flashlist_flutter/lib/src/features/serverpod/application/serverpod_controller.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'package:flashlist_client/flashlist_client.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+
+part 'serverpod_controller.g.dart';
+
+class ServerpodController {
+  ServerpodController(this.ref) {
+    sessionManager = SessionManager(
+      caller: client.modules.auth,
+    );
+    sessionManager.initialize();
+  }
+
+  final Ref ref;
+
+  final client = Client(
+    // The Android endpoint for Genymotion is 10.0.3.2
+    "http://${Platform.isAndroid ? "10.0.3.2" : "localhost"}:8080/",
+    authenticationKeyManager: FlutterAuthenticationKeyManager(),
+  )..connectivityMonitor = FlutterConnectivityMonitor();
+
+  late SessionManager sessionManager;
+  SessionManager get session => sessionManager;
+}
+
+@riverpod
+ServerpodController serverpodController(ServerpodControllerRef ref) =>
+    ServerpodController(ref);
+
+@riverpod
+Client client(ClientRef ref) => ref.read(serverpodControllerProvider).client;
+
+@riverpod
+SessionManager sessionManager(SessionManagerRef ref) =>
+    ref.read(serverpodControllerProvider).sessionManager;
+
+@riverpod
+Stream<dynamic> isAuthenticated(IsAuthenticatedRef ref) async* {
+  final sessionManager = ref.read(serverpodControllerProvider).sessionManager;
+
+  sessionManager.addListener(() {
+    ref.invalidateSelf();
+  });
+
+  yield sessionManager.signedInUser;
+}
---
+++ flashlist_flutter/lib/src/features/serverpod/application/serverpod_controller.g.dart
@@ -0,0 +1,71 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'serverpod_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$serverpodControllerHash() =>
+    r'194254c3256a2bf4394ce63ef03cdac3c28ec379';
+
+/// See also [serverpodController].
+@ProviderFor(serverpodController)
+final serverpodControllerProvider =
+    AutoDisposeProvider<ServerpodController>.internal(
+  serverpodController,
+  name: r'serverpodControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$serverpodControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ServerpodControllerRef = AutoDisposeProviderRef<ServerpodController>;
+String _$clientHash() => r'58f3eea9d2bfedd3947bb3735989a791e1fb0054';
+
+/// See also [client].
+@ProviderFor(client)
+final clientProvider = AutoDisposeProvider<Client>.internal(
+  client,
+  name: r'clientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$clientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ClientRef = AutoDisposeProviderRef<Client>;
+String _$sessionManagerHash() => r'bc940df4fb45528c4315d391a8c4dc32892abf1c';
+
+/// See also [sessionManager].
+@ProviderFor(sessionManager)
+final sessionManagerProvider = AutoDisposeProvider<SessionManager>.internal(
+  sessionManager,
+  name: r'sessionManagerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sessionManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SessionManagerRef = AutoDisposeProviderRef<SessionManager>;
+String _$isAuthenticatedHash() => r'3c969ebf801e2e3156311195fe82f74182f4456a';
+
+/// See also [isAuthenticated].
+@ProviderFor(isAuthenticated)
+final isAuthenticatedProvider = AutoDisposeStreamProvider<dynamic>.internal(
+  isAuthenticated,
+  name: r'isAuthenticatedProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$isAuthenticatedHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef IsAuthenticatedRef = AutoDisposeStreamProviderRef<dynamic>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
