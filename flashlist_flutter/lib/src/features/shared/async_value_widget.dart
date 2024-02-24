4f32bb662b3c01c71a89e7dabb79dd82c879fc4b
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-24 11:01:55 +0100

    create basic async-value-widget

---
+++ flashlist_flutter/lib/src/features/shared/async_value_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AsyncValueWidget<T> extends StatelessWidget {
+  /// A widget that automates the [loading] and [error] state of an [AsyncValue].
+  /// This way it's only necessary to provide the [data] state.
+  /// Intended to be used with [AsyncValue] from the `hooks_riverpod` package.
+  /// Example: `AsyncValueWidget(value: ref.watch(myProvider), data: (data) => Text(data))`
+  const AsyncValueWidget({super.key, required this.value, required this.data});
+  final AsyncValue<T> value;
+  final Widget Function(T data) data;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
+      data: data,
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      error: (e, st) => Center(
+        child: Text(e.toString()),
+      ),
+    );
+  }
+}
