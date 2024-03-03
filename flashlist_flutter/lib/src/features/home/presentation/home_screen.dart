eea5f6a7f44d80c4f150455fe19c40a0411ea955
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-03 09:39:28 +0100

    make home and auth, individual screens

--- flashlist_flutter/lib/src/features/home/presentation/home_screen.dart
+++ flashlist_flutter/lib/src/features/home/presentation/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flashlist_flutter/src/features/authentication/application/authentication.dart';
-import 'package:flashlist_flutter/src/features/authentication/presentation/auth.dart';
+import 'package:flashlist_flutter/src/features/authentication/presentation/auth_screen.dart';
 import 'package:flashlist_flutter/src/features/serverpod/application/serverpod_controller.dart';
+import 'package:flashlist_flutter/src/features/shared/side_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -9,48 +10,22 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final sessionManager = ref.read(serverpodControllerProvider).sessionManager;
-    final isAuthenticated = ref.watch(isAuthenticatedProvider);
+    // final sessionManager = ref.read(sessionManagerProvider);
+    // final isAuthenticated = ref.watch(isAuthenticatedProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Home')),
+        title: const Center(
+          child: Text('Home'),
+        ),
       ),
-      body: isAuthenticated.when(
-        data: (userInfo) {
-          if (userInfo == null) {
-            return const AuthButtons();
-          } else {
-            // TODO: Implement sibling of AuthButtons for authenticated users
-            // So Home-Screen should return either AuthButtons or "FlashlistListScreen"
-            return Column(
-              children: [
-                const Center(
-                  child: Text('Welcome!'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    sessionManager.signOut();
-                  },
-                  child: const Text('Sign Out'),
-                ),
-              ],
-            );
-          }
-        },
-        error: (error, stackTrace) => Scaffold(
-          appBar: AppBar(
-            title: const Text('Error'),
+      drawer: const SideDrawer(),
+      body: const Column(
+        children: [
+          Center(
+            child: Text('Welcome!'),
           ),
-          body: const Center(
-            child: Text('Error'),
-          ),
-        ),
-        loading: () => const Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
-        ),
+        ],
       ),
     );
   }
