e9eca3fcd7541a89a5292b9e0accff5d4e5e16e5
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-03 09:44:51 +0100

    add basic side drawer holding logout option

---
+++ flashlist_flutter/lib/src/features/shared/side_drawer.dart
@@ -0,0 +1,35 @@
+import 'package:flashlist_flutter/src/features/serverpod/application/serverpod_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SideDrawer extends ConsumerWidget {
+  const SideDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sessionManager = ref.read(sessionManagerProvider);
+    final serverpod = ref.read(serverpodControllerProvider);
+    return Drawer(
+      shape: const BeveledRectangleBorder(),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Sign Out'),
+            onTap: () {
+              sessionManager.signOut();
+              context.pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
