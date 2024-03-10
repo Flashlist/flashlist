b46c8c46689f0a4ea194547efef9daa3bcdaa4e7
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-10 15:38:34 +0100

    move shared folder out of features
    shouldn't have been there to begin with

--- flashlist_flutter/lib/src/shared/async_value_widget.dart
+++ flashlist_flutter/lib/src/shared/async_value_widget.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AsyncValueWidget<T> extends StatelessWidget {
-  /// A widget that automates the [loading] and [error] state of an [AsyncValue].
-  /// This way it's only necessary to provide the [data] state.
-  /// Intended to be used with [AsyncValue] from the `hooks_riverpod` package.
+/// A widget that automates the [loading] and [error] state of an [AsyncValue].
+/// This way it's only necessary to provide the [data] state.
+/// Intended to be used with [AsyncValue] from the `hooks_riverpod` package.
 
-  /// Usage:
-  /// ```dart
-  /// final AsyncValueWidget(value: ref.watch(myProvider), data: (data) => Text(data));
-  /// ```
+/// Usage:
+/// ```dart
+/// final AsyncValueWidget(value: ref.watch(myProvider), data: (data) => Text(data));
+/// ```
+class AsyncValueWidget<T> extends StatelessWidget {
   const AsyncValueWidget({super.key, required this.value, required this.data});
   final AsyncValue<T> value;
   final Widget Function(T data) data;
