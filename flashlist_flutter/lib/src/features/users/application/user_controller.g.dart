de5bc0d54f45cac7fbc1918bb02a808454149e2f
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-12 19:03:45 +0100

    add user_controller

---
+++ flashlist_flutter/lib/src/features/users/application/user_controller.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userControllerHash() => r'c223e79ac52d06ff6f2077678e394a8933dd7768';
+
+/// See also [userController].
+@ProviderFor(userController)
+final userControllerProvider = AutoDisposeProvider<UserController>.internal(
+  userController,
+  name: r'userControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UserControllerRef = AutoDisposeProviderRef<UserController>;
+String _$currentUserHash() => r'393c958ba9ff01c1327b28de02a193e846ff589f';
+
+/// See also [currentUser].
+@ProviderFor(currentUser)
+final currentUserProvider = AutoDisposeFutureProvider<AppUser?>.internal(
+  currentUser,
+  name: r'currentUserProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$currentUserHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CurrentUserRef = AutoDisposeFutureProviderRef<AppUser?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
