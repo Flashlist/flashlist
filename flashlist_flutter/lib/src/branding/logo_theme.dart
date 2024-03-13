f1336c4036348baf61220c061d44870fe6077b5b
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-13 12:29:31 +0100

    add theme and theme helper

---
+++ flashlist_flutter/lib/src/branding/logo_theme.dart
@@ -0,0 +1,1 @@
+enum LogoTheme { black, white, color }
---
+++ flashlist_flutter/lib/src/utils/context_helper.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+ThemeData themeOf(context) {
+  return Theme.of(context);
+}
+
+TextTheme textThemeOf(context) {
+  return themeOf(context).textTheme;
+}
+
+ColorScheme colorSchemeOf(context) {
+  return themeOf(context).colorScheme;
+}
+
+Brightness brightnessOf(context) {
+  return themeOf(context).brightness;
+}
+
+bool isDarkThemeOf(context) {
+  return brightnessOf(context) == Brightness.dark;
+}
+
+void showContextSnackBar({
+  context,
+  required String message,
+  SnackBarAction? action,
+}) {
+  ScaffoldMessenger.of(context).clearSnackBars();
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      action: action,
+      backgroundColor: colorSchemeOf(context).primary,
+      content: Text(message),
+    ),
+  );
+}
