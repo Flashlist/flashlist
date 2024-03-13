9b2d0fc0c6cbe754e13b08fcdb1a4ee435aaf7d3
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-13 12:31:18 +0100

    implement adaptive theme dark-first

--- flashlist_flutter/lib/main.dart
+++ flashlist_flutter/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flashlist_flutter/src/features/routing/app_router.dart';
+import 'package:flashlist_flutter/src/theme/app_theme_dark.dart';
+import 'package:flashlist_flutter/src/theme/app_theme_light.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,13 +16,22 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, this.savedThemeMode});
+
+  final AdaptiveThemeMode? savedThemeMode;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flashlist',
-      routerConfig: goRouter,
+    return AdaptiveTheme(
+      light: lightTheme,
+      dark: darkTheme,
+      initial: savedThemeMode ?? AdaptiveThemeMode.dark,
+      builder: (theme, darkTheme) => MaterialApp.router(
+        title: 'Flashlist',
+        theme: theme,
+        darkTheme: darkTheme,
+        routerConfig: goRouter,
+      ),
     );
   }
 }
