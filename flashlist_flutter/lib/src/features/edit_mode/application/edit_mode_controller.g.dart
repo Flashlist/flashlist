b3d88abdee3360df266c28e27ea0ce689f62de2b
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-17 12:37:08 +0100

    add EditModeController

---
+++ flashlist_flutter/lib/src/features/edit_mode/application/edit_mode_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'edit_mode_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$editModeControllerHash() =>
+    r'715f810c51dd26d4aed642e2e0431e73fa64126c';
+
+/// See also [EditModeController].
+@ProviderFor(EditModeController)
+final editModeControllerProvider =
+    AutoDisposeNotifierProvider<EditModeController, EditModeState>.internal(
+  EditModeController.new,
+  name: r'editModeControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$editModeControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EditModeController = AutoDisposeNotifier<EditModeState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
