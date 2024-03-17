452fdba8090d1b9ed758286049a534ebcb0253e8
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-17 12:37:58 +0100

    add ColorPickerController

---
+++ flashlist_flutter/lib/src/features/color_picker/application/color_picker_controller.dart
@@ -0,0 +1,62 @@
+import 'package:flashlist_flutter/src/features/color_picker/domain/color_picker_state.dart';
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'color_picker_controller.g.dart';
+
+@riverpod
+
+/// State Notifier class for the color picker
+///
+/// Allows to manipulate the color picker state through,
+///
+/// [changeAlpha], [changeHue], [changeSaturation], [changeVValue], [takeHex]
+///
+/// It can be called from anywhere in the app and will update the state
+class ColorPickerController extends _$ColorPickerController {
+  @override
+  ColorPickerState build() {
+    return const ColorPickerState(hue: 0.0);
+  }
+
+  // Alpha = Opacity
+  void changeAlpha(double value) {
+    state = state.copyWith(
+      alpha: value,
+    );
+  }
+
+  void changeHue(double value) {
+    state = state.copyWith(
+      hue: value,
+    );
+  }
+
+  void changeSaturation(double value) {
+    state = state.copyWith(
+      saturation: value,
+    );
+  }
+
+  void changeVValue(double value) {
+    state = state.copyWith(
+      vValue: value,
+    );
+  }
+
+  void takeHex(String hex) async {
+    final hexValue = hex.startsWith('#') ? hex.substring(1) : hex;
+
+    final hsvColor = HSVColor.fromColor(
+      Color(int.parse('0xFF$hexValue')),
+    );
+
+    state = state.copyWith(
+      hue: hsvColor.hue,
+      saturation: hsvColor.saturation,
+      vValue: hsvColor.value,
+      color: hsvColor,
+      colorHexValue: hexValue,
+    );
+  }
+}
---
+++ flashlist_flutter/lib/src/features/color_picker/application/color_picker_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'color_picker_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$colorPickerControllerHash() =>
+    r'ef801439364d7afe9ec55b1c6b7c98884f8e9afb';
+
+/// See also [ColorPickerController].
+@ProviderFor(ColorPickerController)
+final colorPickerControllerProvider = AutoDisposeNotifierProvider<
+    ColorPickerController, ColorPickerState>.internal(
+  ColorPickerController.new,
+  name: r'colorPickerControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$colorPickerControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ColorPickerController = AutoDisposeNotifier<ColorPickerState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
