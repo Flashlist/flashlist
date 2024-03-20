f5f18640b11fe32020631ed1882c73663342f1fe
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-20 10:32:33 +0100

    add update logic to flashlistStreamMessageHandler
    and comments

--- flashlist_flutter/lib/src/features/flashlist/application/flashlist_stream_message_handler.dart
+++ flashlist_flutter/lib/src/features/flashlist/application/flashlist_stream_message_handler.dart
@@ -10,15 +10,38 @@ void handleFlashlistStreamMessage(
   List<Flashlist?> streamItems,
   SerializableEntity message,
 ) {
+  /// [FlashlistBatch] is a message that contains a collection of [Flashlist]
+  /// entities. It is sent when the user first connects to the WebSocket.
   if (message is FlashlistBatch) {
     streamItems.addAll(message.collection);
   }
 
+  /// [Flashlist] is a message that contains a single [Flashlist] entity.
   if (message is Flashlist) {
     streamItems.add(message);
   }
 
+  /// [DeleteFlashlist] is a message that contains the [id] of a [Flashlist]
+  /// entity that should be removed from the list.
   if (message is DeleteFlashlist) {
     streamItems.removeWhere((element) => element!.id == message.flashlistId);
+  }
+
+  /// [UpdateFlashlist] is a message that contains the [id] of a [Flashlist]
+  /// entity that should be updated. It also contains the new [title] and [color]
+  /// of the [Flashlist].
+  if (message is UpdateFlashlist) {
+    final indexToRemove =
+        streamItems.indexWhere((element) => element!.id == message.id);
+    final itemToRemove = streamItems[indexToRemove];
+
+    streamItems.removeAt(indexToRemove);
+    streamItems.insert(
+      indexToRemove,
+      itemToRemove!.copyWith(
+        title: message.title,
+        color: message.color,
+      ),
+    );
   }
 }
