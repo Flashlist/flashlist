e4d1caef54ecb5509ff093cbb7683a807305d1d0
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-21 12:00:36 +0100

    move flashlist card and title

---
+++ flashlist_flutter/lib/src/features/flashlist/presentation/flashlist/flashlist_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:flashlist_client/flashlist_client.dart';
+import 'package:flashlist_flutter/src/constants/app_sizes.dart';
+import 'package:flashlist_flutter/src/features/color_picker/application/color_picker_controller.dart';
+import 'package:flashlist_flutter/src/features/edit_mode/application/edit_mode_panel_controller.dart';
+import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist/flashlist_card_header.dart';
+import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist_items/flashlist_body.dart';
+import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist_items/flashlist_item_input.dart';
+
+/// A widget to display a single flashlist
+/// Includes a [TextField] to add new items to the flashlist
+/// [isAdding] is a bool passed to represent if the TextField is visible
+/// Only one TextField should be visible at a time.
+class FlashlistCard extends HookConsumerWidget {
+  const FlashlistCard({
+    super.key,
+    required this.flashlist,
+    required this.isAdding,
+    required this.toggleIsAdding,
+  });
+
+  final Flashlist flashlist;
+  final bool isAdding;
+  final Function() toggleIsAdding;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Get the editModePanelState
+    final editModePanel = ref.watch(editModePanelControllerProvider);
+
+    // Get the colorPickerState
+    final colorPicker = ref.watch(colorPickerControllerProvider);
+
+    // Get the color of the flashlist
+    // If the flashlist is in edit mode, use the color from the colorPicker
+    // Otherwise, use the color from the flashlist
+    final flashlistColor = editModePanel.flashlistInEditMode == flashlist.id
+        ? colorPicker.color.toColor()
+        : Color(int.parse(flashlist.color));
+
+    return Column(
+      children: [
+        AnimatedContainer(
+          margin: const EdgeInsets.all(Sizes.p8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: flashlistColor,
+          ),
+          duration: Duration.zero,
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                gapH8,
+                FlashlistCardHeader(flashlist: flashlist),
+                gapH12,
+                FlashlistBody(flashlist: flashlist),
+                gapH12,
+                IconButton(
+                  onPressed: toggleIsAdding,
+                  icon: isAdding
+                      ? const Icon(Icons.arrow_upward)
+                      : const Icon(Icons.add),
+                ),
+                gapH8,
+              ],
+            ),
+          ),
+        ),
+        // Only visible when isAdding is true
+        FlashlistItemInput(flashlist: flashlist, isAdding: isAdding),
+      ],
+    );
+  }
+}
