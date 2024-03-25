29aa9a9000ed0ec74bf784b2ca3d4f9f6652295f
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-25 12:52:55 +0100

    moved FlashlistCardHeader specific code into own dir

--- flashlist_flutter/lib/src/features/flashlist/presentation/flashlist/flashlist_card_header/flashlist_card_header.dart
+++ flashlist_flutter/lib/src/features/flashlist/presentation/flashlist/flashlist_card_header/flashlist_card_header.dart
@@ -1,8 +1,9 @@
+import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist/flashlist_card_header/avatar_group.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flashlist_client/flashlist_client.dart';
-import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist/flashlist_menu.dart';
-import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist/flashlist_title.dart';
+import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist/flashlist_card_header/flashlist_menu.dart';
+import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist/flashlist_card_header/flashlist_title.dart';
 
 /// A widget to display the header of a flashlist.
 /// Includes the title consisting of [Text] and [TextField] widgets
@@ -23,7 +24,7 @@ class FlashlistCardHeader extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Text('avatar'),
+        AvatarGroup(flashlist.authors ?? []),
         FlashlistTitle(flashlist: flashlist),
         FlashlistMenu(flashlist),
       ],
