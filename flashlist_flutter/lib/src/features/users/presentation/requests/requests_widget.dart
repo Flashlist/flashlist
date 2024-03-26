1c7f5a2c893f547ba3c85f359250ff9bec534ad9
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-26 12:53:44 +0100

    add confirm dialog informing user of destructive action

--- flashlist_flutter/lib/src/features/users/presentation/requests/requests_widget.dart
+++ flashlist_flutter/lib/src/features/users/presentation/requests/requests_widget.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'package:flashlist_client/flashlist_client.dart';
 import 'package:flashlist_flutter/src/features/flashlist/application/flashlist_controller.dart';
 import 'package:flashlist_flutter/src/features/users/application/user_controller.dart';
 import 'package:flashlist_flutter/src/shared/async_value_widget.dart';
-import 'package:flashlist_flutter/src/utils/serverpod/serverpod_helper.dart';
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class RequestsWidget extends ConsumerWidget {
   const RequestsWidget({super.key});
@@ -14,7 +14,7 @@ class RequestsWidget extends ConsumerWidget {
     return AsyncValueWidget(
         value: ref.watch(pendingRequestsProvider('join_flashlist')),
         data: (requests) {
-          if (requests == null || requests.isEmpty) {
+          if (requests.isEmpty) {
             return const Center(
               child: Text('No pending requests.'),
             );
@@ -28,9 +28,14 @@ class RequestsWidget extends ConsumerWidget {
                   itemCount: requests.length,
                   itemBuilder: (context, index) {
                     final request = requests[index];
+                    final listToJoin = ref.watch(
+                      flashlistByIdProvider(
+                        int.parse(request!.data!),
+                      ),
+                    );
 
                     return ListTile(
-                      title: Text('Join request to ${request!.data}'),
+                      title: Text('Join request to ${listToJoin.value?.title}'),
                       subtitle: const Text(
                         'invitation',
                       ),
