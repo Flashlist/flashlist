3d695fd7ce6abfc6e72997924843ca681c563f01
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-26 13:53:20 +0100

    add info to ShareWithEmail

--- flashlist_flutter/lib/src/features/flashlist/presentation/share/share_via_email.dart
+++ flashlist_flutter/lib/src/features/flashlist/presentation/share/share_via_email.dart
@@ -1,5 +1,8 @@
+import 'package:flashlist_flutter/src/constants/app_sizes.dart';
 import 'package:flashlist_flutter/src/utils/serverpod/serverpod_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:flashlist_client/flashlist_client.dart';
@@ -23,25 +26,45 @@ class ShareViaEmail extends ConsumerWidget {
           userId: null,
         ),
       );
-      Navigator.pop(context);
+      context.pop();
     }
 
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: Sizes.p8),
+      height: 40,
       child: Column(
         children: [
-          const SizedBox(height: 20),
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Email',
-              border: OutlineInputBorder(),
-            ),
-            controller: emailController,
-            onSubmitted: (_) => submitInvitation(),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: emailController,
+                  onSubmitted: (_) => submitInvitation(),
+                ),
+              ),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                onPressed: submitInvitation,
+                child: const Text('Send'),
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: submitInvitation,
-            child: const Text('Send'),
+          gapH24,
+          Container(
+            padding: const EdgeInsets.all(Sizes.p8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: const Text(
+              'You can invite a user here via email. If a user with the provided email exists, they will receive your invitation',
+              style: TextStyle(fontSize: Sizes.p16),
+            ),
           ),
         ],
       ),
