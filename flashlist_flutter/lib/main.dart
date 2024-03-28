953cbc79369a9901bc1e89a5b107a4b73f9cd743
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-28 19:27:04 +0100

    set up l10n

--- flashlist_flutter/lib/main.dart
+++ flashlist_flutter/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:flashlist_flutter/src/features/routing/app_router.dart';
 import 'package:flashlist_flutter/src/theme/app_theme_dark.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flashlist_flutter/src/theme/app_theme_light.dart';
 
 void main() async {
@@ -32,6 +34,17 @@ class MyApp extends StatelessWidget {
         theme: theme,
         darkTheme: darkTheme,
         routerConfig: goRouter,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'), // English
+          Locale('es'), // Spanish
+          Locale('de'), // German
+        ],
       ),
     );
   }
