27d883f4653054205904e00ebc1a6fde7f44bd2e
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-31 16:01:15 +0200

    add SettingsScreen

---
+++ flashlist_flutter/lib/src/features/settings/presentation/settings_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SettingsScreen extends ConsumerWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: const Center(
+        child: Text('Settings Screen'),
+      ),
+    );
+  }
+}
