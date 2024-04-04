f69e4eb2ee0b771669c1d812bdee4877c161912a
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-04-04 17:15:19 +0200

    replace placeholders with List of recent colors  with gesturedetector

--- flashlist_flutter/lib/src/features/color_picker/presentation/advanced_color.dart/recent_colors.dart
+++ flashlist_flutter/lib/src/features/color_picker/presentation/advanced_color.dart/recent_colors.dart
@@ -1,29 +1,45 @@
-import 'package:flashlist_flutter/src/constants/app_sizes.dart';
-import 'package:flashlist_flutter/src/utils/context_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class RecentColors extends StatelessWidget {
+import 'package:flashlist_flutter/src/constants/app_sizes.dart';
+import 'package:flashlist_flutter/src/features/color_picker/application/color_picker_controller.dart';
+import 'package:flashlist_flutter/src/utils/context_helper.dart';
+import 'package:flashlist_flutter/src/utils/local_storage/local_storage_helper.dart';
+
+/// RecentColors is a row of 8 colors that the user has recently used
+/// Should be saved either in local storage or in a database
+class RecentColors extends ConsumerWidget {
   const RecentColors({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    /// RecentColors is a row of 8 colors that the user has recently used
-    /// Should be saved either in local storage or in a database
-    /// TODO: implement saving recent colors
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colors =
+        ref.watch(localStorageHelperProvider).recentColors.reversed.toList();
+
     return Expanded(
-      child: Row(
-        children: [
-          for (var i = 0; i < 8; i++)
-            Container(
+      child: ListView.builder(
+        itemCount: 8,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final color = colors.length > index
+              ? Color(int.parse(colors[index]))
+              : Colors.transparent;
+
+          return GestureDetector(
+            onTap: () {
+              ref
+                  .read(colorPickerControllerProvider.notifier)
+                  .takeInt(int.parse(colors[index]));
+              ref
+                  .read(colorPickerControllerProvider.notifier)
+                  .takeInt(int.parse(colors[index]));
+            },
+            child: Container(
               decoration: BoxDecoration(
                 border: Border.all(
                   color: colorSchemeOf(context).onBackground,
                 ),
-                color: Color.lerp(
-                  Colors.teal,
-                  Colors.purple,
-                  i / 8,
-                ),
+                color: color,
                 borderRadius: const BorderRadius.all(
                   Radius.circular(6),
                 ),
@@ -31,7 +47,8 @@ class RecentColors extends StatelessWidget {
               width: (mediaQueryOf(context).size.width - 28) / 8,
               height: Sizes.p40,
             ),
-        ],
+          );
+        },
       ),
     );
   }
