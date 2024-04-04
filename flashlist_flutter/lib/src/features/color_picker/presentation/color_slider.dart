bf630d2667f237f8b744134a56de97cda01f7bff
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-04-04 17:12:49 +0200

    add color-gradient overlay to ColorSlider

--- flashlist_flutter/lib/src/features/color_picker/presentation/color_slider.dart
+++ flashlist_flutter/lib/src/features/color_picker/presentation/color_slider.dart
@@ -1,6 +1,8 @@
-import 'package:flashlist_flutter/src/features/color_picker/application/color_picker_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:flashlist_flutter/src/features/color_picker/application/color_picker_controller.dart';
 
 enum ColorPickerValue {
   alpha,
@@ -8,7 +10,7 @@ enum ColorPickerValue {
   saturation,
 }
 
-class ColorSlider extends ConsumerWidget {
+class ColorSlider extends HookConsumerWidget {
   /// Slider widget for the color picker
   /// It uses the [ColorPickerController] to update the color
   /// The [parameter] is the value that will be updated
@@ -24,14 +26,37 @@ class ColorSlider extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(colorPickerControllerProvider);
 
+    final isChanging = useState(false);
+
     void changeValue(newValue) =>
         ref.read(colorPickerControllerProvider.notifier).changeHue(newValue);
 
-    return Slider.adaptive(
-      value: state.hue,
-      min: 0,
-      max: parameter == ColorPickerValue.hue ? 360 : 1.0,
-      onChanged: (newValue) => changeValue(newValue),
+    return Stack(
+      children: [
+        AnimatedOpacity(
+          duration: const Duration(milliseconds: 200),
+          opacity: isChanging.value ? 1.0 : 0.0,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              gradient: LinearGradient(
+                colors: [
+                  for (var h = 0.0; h < 360.0; h += 1)
+                    HSVColor.fromAHSV(1.0, h, 1.0, 1.0).toColor()
+                ],
+              ),
+            ),
+          ),
+        ),
+        Slider.adaptive(
+          onChangeStart: (value) => isChanging.value = true,
+          onChangeEnd: (value) => isChanging.value = false,
+          value: state.hue,
+          min: 0,
+          max: parameter == ColorPickerValue.hue ? 360 : 1.0,
+          onChanged: (newValue) => changeValue(newValue),
+        ),
+      ],
     );
   }
 }
