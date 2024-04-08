1313f87168ce4bd6f018481b0b4098603767f93c
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-04-08 13:57:05 +0200

    add indicator to notification badge

--- flashlist_flutter/lib/src/features/home/presentation/notification_badge.dart
+++ flashlist_flutter/lib/src/features/home/presentation/notification_badge.dart
@@ -1,4 +1,7 @@
+import 'package:flashlist_flutter/src/constants/app_sizes.dart';
 import 'package:flashlist_flutter/src/features/routing/app_router.dart';
+import 'package:flashlist_flutter/src/features/users/application/user_controller.dart';
+import 'package:flashlist_flutter/src/shared/async_value_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -8,11 +11,32 @@ class NotificationBadge extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return IconButton(
-      onPressed: () {
-        context.goNamed(AppRoute.connections.name);
-      },
-      icon: const Icon(Icons.notifications),
-    );
+    return AsyncValueWidget(
+        value: ref.watch(pendingRequestsProvider(null)),
+        data: (pendingRequests) {
+          return Stack(
+            children: [
+              IconButton(
+                onPressed: () {
+                  context.goNamed(AppRoute.connections.name);
+                },
+                icon: const Icon(Icons.notifications),
+              ),
+              if (pendingRequests.isNotEmpty)
+                Positioned(
+                  top: Sizes.p16,
+                  right: Sizes.p16,
+                  child: Container(
+                    width: Sizes.p8,
+                    height: Sizes.p8,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+            ],
+          );
+        });
   }
 }
