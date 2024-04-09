97105308b1ce7f84749d0b75451e0ced50ebe312
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-04-09 13:09:27 +0200

    add AppVersionInfo (ListTile)

---
+++ flashlist_flutter/lib/src/features/settings/presentation/settings_items/app_version_info.dart
@@ -0,0 +1,22 @@
+import 'package:flashlist_flutter/src/shared/async_value_widget.dart';
+import 'package:flashlist_flutter/src/utils/context_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:flashlist_flutter/src/features/settings/application/settings_controller.dart';
+
+class AppVersionInfo extends ConsumerWidget {
+  const AppVersionInfo({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AsyncValueWidget(
+        value: ref.watch(appPackageInfoProvider),
+        data: (packageInfo) {
+          return ListTile(
+            title: Text(localizationsOf(context).appVersion),
+            subtitle: Text(packageInfo.version),
+          );
+        });
+  }
+}
