f9edc200d4043c2d49b5e0a882e424b687fbbabb
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-04-24 16:14:49 +0200

    also instantiate helper in home screen

--- flashlist_flutter/lib/src/features/home/presentation/home_screen.dart
+++ flashlist_flutter/lib/src/features/home/presentation/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flashlist_flutter/src/features/home/presentation/notification_badge.dart';
+import 'package:flashlist_flutter/src/utils/serverpod/serverpod_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -18,6 +19,7 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(serverpodHelperProvider);
     final editModeController = ref.watch(editModePanelControllerProvider);
     final flashlistController = ref.watch(flashlistControllerProvider);
 
