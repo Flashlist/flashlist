261e47d224355bbdeaf274dd4b1a1e5fd5ad0fc1
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-05-30 14:18:44 +0200

    fix imports

--- flashlist_flutter/lib/src/features/edit_mode/presentation/edit_mode_controls.dart
+++ flashlist_flutter/lib/src/features/edit_mode/presentation/edit_mode_controls.dart
@@ -1,13 +1,12 @@
-import 'package:flashlist_flutter/src/constants/app_sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:flashlist_client/flashlist_client.dart';
+import 'package:flashlist_flutter/src/constants/app_sizes.dart';
 import 'package:flashlist_flutter/src/features/color_picker/application/color_picker_controller.dart';
 import 'package:flashlist_flutter/src/features/edit_mode/application/edit_mode_controller.dart';
 import 'package:flashlist_flutter/src/features/edit_mode/application/edit_mode_panel_controller.dart';
 import 'package:flashlist_flutter/src/features/flashlist/application/flashlist_controller.dart';
-import 'package:flashlist_flutter/src/utils/context_helper.dart';
 import 'package:flashlist_flutter/src/utils/local_storage/local_storage_helper.dart';
 
 /// Controls buttons for Edit Mode Panel
