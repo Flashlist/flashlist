230092c5ec4055cf029d8b00a8357a82230d1211
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-05-30 14:17:48 +0200

    refactor FlashlistItemDeletionHandler
    Now it doesn't debounce, the item is directly deleted and the ReInsert method creates a new item with the same properties at the same order nr
    
    ID will be a new one though!

--- flashlist_flutter/lib/src/features/flashlist/presentation/flashlist_items/mixins/flashlist_item_deletion_handler.dart
+++ flashlist_flutter/lib/src/features/flashlist/presentation/flashlist_items/mixins/flashlist_item_deletion_handler.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:flashlist_client/flashlist_client.dart';
+import 'package:flashlist_flutter/src/features/flashlist/application/flashlist_controller.dart';
 import 'package:flashlist_flutter/src/features/flashlist/presentation/flashlist_items/flashlist_body.dart';
 import 'package:flashlist_flutter/src/utils/context_helper.dart';
 
@@ -12,14 +11,6 @@ import 'package:flashlist_flutter/src/utils/context_helper.dart';
 /// Shows a SnackBar with an undo action for 5 seconds
 /// If the action is not undone, the item is deleted
 mixin FlashlistItemDeletionHandler on ConsumerState<FlashlistBody> {
-  Completer<void>? completer;
-  late Timer timer;
-  late FlashlistItem pendingItem;
-  int? pendingItemIndex;
-
-  /// Gets the FlashlistItems from the consuming class
-  List<FlashlistItem?> getFlashlistItems();
-
   /// Deletes the FlashlistItem on the remote from the consuming class
   void deleteItemOnRemote(int itemId, int parentId);
 
@@ -30,28 +21,12 @@ mixin FlashlistItemDeletionHandler on ConsumerState<FlashlistBody> {
     BuildContext context,
     FlashlistItem item,
   ) {
-    List<FlashlistItem?> flashlistItems = getFlashlistItems();
-
-    // If there is a pending item, delete it and cancel the timer
-    if (completer != null && !completer!.isCompleted) {
-      timer.cancel();
-      deleteItemOnRemote(item.id!, item.parentId);
-    }
-
-    completer = Completer<void>();
-    pendingItem = item;
-    pendingItemIndex = flashlistItems.indexOf(item);
-
-    setState(() {
-      flashlistItems.remove(item);
-    });
-
-    timer = Timer(const Duration(seconds: 3), () {
-      if (!completer!.isCompleted) {
-        deleteItemOnRemote(item.id!, item.parentId);
-        completer!.complete();
-      }
-    });
+    ref.read(flashlistControllerProvider).deleteFlashlistItem(
+          DeleteFlashlistItem(
+            id: item.id!,
+            parentId: item.parentId,
+          ),
+        );
 
     context.showContextSnackBar(
       context,
@@ -60,17 +35,11 @@ mixin FlashlistItemDeletionHandler on ConsumerState<FlashlistBody> {
       action: SnackBarAction(
         label: 'Undo',
         onPressed: () {
-          if (!completer!.isCompleted) {
-            setState(() {
-              if (flashlistItems.length < item.orderNr) {
-                flashlistItems.add(item);
-              } else {
-                flashlistItems.insert(pendingItemIndex!, item);
-              }
-            });
-            completer!.complete();
-            timer.cancel();
-          }
+          ref.read(flashlistControllerProvider).insertFlashlistItem(
+                InsertFlashlistItem(
+                  item: item,
+                ),
+              );
         },
       ),
     );
