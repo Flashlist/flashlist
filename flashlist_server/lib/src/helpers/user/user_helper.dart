84f5147c291427f9a2c0300e9ea8abe73f17b4ef
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-06-11 17:45:55 +0200

    add method to delete user serverside

--- flashlist_server/lib/src/helpers/user/user_helper.dart
+++ flashlist_server/lib/src/helpers/user/user_helper.dart
@@ -1,5 +1,6 @@
 import 'package:flashlist_server/src/generated/protocol.dart';
 import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_auth_server/serverpod_auth_server.dart';
 
 /// Helper Class for user-specific methods
 /// that are shared among endpoints.
@@ -38,5 +39,90 @@ class UserHelper {
     } catch (e) {
       throw Exception('Failed to get user by email: $e');
     }
+  }
+
+  Future<bool> deleteAccount(Session session) async {
+    final int? userId = await getAuthenticatedUserId(session);
+    if (userId == null) {
+      return false;
+    }
+
+    AppUser? appUser = await AppUser.db.findFirstRow(
+      session,
+      where: (user) => user.id.equals(userId),
+    );
+
+    if (appUser == null) {
+      return false;
+    }
+
+    UserInfo? user = await UserInfo.db.findById(session, userId);
+
+    if (user == null) {
+      return false;
+    }
+
+    try {
+      final String? email = user.email;
+
+      if (email == null) {
+        return false;
+      }
+
+      await session.db.transaction((transaction) async {
+        await AppUser.db.deleteRow(
+          session,
+          appUser,
+          transaction: transaction,
+        );
+
+        await session.db.unsafeQuery(
+          'DELETE FROM serverpod_user_info WHERE email=\'$email\'',
+          transaction: transaction,
+        );
+
+        await session.db.unsafeQuery(
+          'DELETE FROM serverpod_email_auth WHERE email=\'$email\'',
+          transaction: transaction,
+        );
+
+        await session.db.unsafeQuery(
+          'DELETE FROM serverpod_email_create_request WHERE email=\'$email\'',
+          transaction: transaction,
+        );
+
+        await session.db.unsafeQuery(
+          'DELETE FROM serverpod_email_failed_sign_in WHERE email=\'$email\'',
+          transaction: transaction,
+        );
+
+        ColumnInt columnInt =
+            ColumnInt('userId', Table(tableName: 'serverpod_email_reset'));
+
+        await session.db.unsafeQuery(
+          'DELETE FROM serverpod_email_reset WHERE $columnInt=\'$userId\'',
+          transaction: transaction,
+        );
+
+        ColumnInt columnInt2 =
+            ColumnInt('userId', Table(tableName: 'serverpod_auth_key'));
+
+        await session.db.unsafeQuery(
+          'DELETE FROM serverpod_auth_key WHERE $columnInt2=\'$userId\'',
+          transaction: transaction,
+        );
+
+        await session.db.unsafeExecute(
+          'DELETE FROM serverpod_email_auth WHERE email=\'$email\'',
+          transaction: transaction,
+        );
+
+        return true;
+      });
+    } catch (e) {
+      print('Failed to delete account: $e');
+      return false;
+    }
+    return true;
   }
 }
